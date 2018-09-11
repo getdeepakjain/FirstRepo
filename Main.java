83f00396bf65ccc83707dcaa295dabb19b53ba5b
getdeepakjain <43176989+getdeepakjain@users.noreply.github.com>
2018-09-11 19:41:43 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Long a = 127L;
+		Long b = 127L;
+		
+		if(a==b) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+	}
+}
